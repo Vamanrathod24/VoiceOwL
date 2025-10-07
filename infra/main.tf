629fce6931f234118f3e6a0815e078655efbd6ec
Vamanrathod24 <vamanrathod2002@gmail.com>
2025-10-07 06:22:28 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -4,51 +4,57 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "eks_vpc" {
+# -------------------------------
+# VPC
+# -------------------------------
+resource "aws_vpc" "voiceowl_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "eks-vpc"
+    Name = "voiceowl-vpc"
   }
 }
 
-resource "aws_subnet" "eks_subnet" {
+resource "aws_subnet" "voiceowl_subnet" {
   count                   = 2
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.voiceowl_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.voiceowl_vpc.cidr_block, 8, count.index)
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
   tags = {
-    Name = "eks-subnet-${count.index}"
+    Name = "voiceowl-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "eks_igw" {
-  vpc_id = aws_vpc.eks_vpc.id
+resource "aws_internet_gateway" "voiceowl_igw" {
+  vpc_id = aws_vpc.voiceowl_vpc.id
   tags = {
-    Name = "eks-igw"
+    Name = "voiceowl-igw"
   }
 }
 
-resource "aws_route_table" "eks_route_table" {
-  vpc_id = aws_vpc.eks_vpc.id
+resource "aws_route_table" "voiceowl_route_table" {
+  vpc_id = aws_vpc.voiceowl_vpc.id
   tags = {
-    Name = "eks-route-table"
+    Name = "voiceowl-route-table"
   }
 }
 
-resource "aws_route" "eks_route" {
-  route_table_id         = aws_route_table.eks_route_table.id
+resource "aws_route" "voiceowl_route" {
+  route_table_id         = aws_route_table.voiceowl_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.eks_igw.id
+  gateway_id             = aws_internet_gateway.voiceowl_igw.id
 }
 
-resource "aws_route_table_association" "eks_subnet_assoc" {
-  count          = length(aws_subnet.eks_subnet)
-  subnet_id      = aws_subnet.eks_subnet[count.index].id
-  route_table_id = aws_route_table.eks_route_table.id
+resource "aws_route_table_association" "voiceowl_subnet_assoc" {
+  count          = length(aws_subnet.voiceowl_subnet)
+  subnet_id      = aws_subnet.voiceowl_subnet[count.index].id
+  route_table_id = aws_route_table.voiceowl_route_table.id
 }
 
-data "aws_iam_policy_document" "eks_assume_role_policy" {
+# -------------------------------
+# IAM Roles for Cluster
+# -------------------------------
+data "aws_iam_policy_document" "voiceowl_assume_role_policy" {
   statement {
     effect = "Allow"
     actions = ["sts:AssumeRole"]
@@ -59,60 +65,82 @@ data "aws_iam_policy_document" "eks_assume_role_policy" {
   }
 }
 
-resource "aws_iam_role" "eks_cluster_role" {
-  name               = "eks-cluster-role"
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+resource "aws_iam_role" "voiceowl_cluster_role" {
+  name               = "voiceowl-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.voiceowl_assume_role_policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
+resource "aws_iam_role_policy_attachment" "voiceowl_cluster_policy" {
+  role       = aws_iam_role.voiceowl_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
+resource "aws_iam_role_policy_attachment" "voiceowl_service_policy" {
+  role       = aws_iam_role.voiceowl_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
 }
 
-resource "aws_eks_cluster" "cluster" {
-  name     = "devsecops-cluster-app"
-  role_arn = aws_iam_role.eks_cluster_role.arn
+# -------------------------------
+# EKS Cluster
+# -------------------------------
+resource "aws_eks_cluster" "voiceowl_cluster" {
+  name     = "voiceowl-cluster"
+  role_arn = aws_iam_role.voiceowl_cluster_role.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.eks_subnet[*].id
+    subnet_ids = aws_subnet.voiceowl_subnet[*].id
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy,
-    aws_iam_role_policy_attachment.eks_service_policy,
+    aws_iam_role_policy_attachment.voiceowl_cluster_policy,
+    aws_iam_role_policy_attachment.voiceowl_service_policy,
   ]
 }
 
-resource "aws_iam_role" "eks_nodegroup_role" {
-  name               = "eks-node-group-role"
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+# -------------------------------
+# IAM Role for Node Group
+# -------------------------------
+data "aws_iam_policy_document" "nodegroup_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]  # MUST include this
+    }
+  }
 }
 
+resource "aws_iam_role" "voiceowl_nodegroup_role" {
+  name               = "voiceowl-nodegroup-role"
+  assume_role_policy = data.aws_iam_policy_document.nodegroup_assume_role_policy.json
+}
+
+# Attach required policies
 resource "aws_iam_role_policy_attachment" "node_group_worker_policy" {
-  role       = aws_iam_role.eks_nodegroup_role.name
+  role       = aws_iam_role.voiceowl_nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "node_group_cni_policy" {
-  role       = aws_iam_role.eks_nodegroup_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSCNIPolicy"
+  role       = aws_iam_role.voiceowl_nodegroup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "node_group_ecr_read_only" {
-  role       = aws_iam_role.eks_nodegroup_role.name
+  role       = aws_iam_role.voiceowl_nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "devsecops-node-group"
-  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
-  subnet_ids      = aws_subnet.eks_subnet[*].id
+
+# -------------------------------
+# EKS Node Group
+# -------------------------------
+resource "aws_eks_node_group" "voiceowl_nodegroup" {
+  cluster_name    = aws_eks_cluster.voiceowl_cluster.name
+  node_group_name = "voiceowl-node-group"
+  node_role_arn   = aws_iam_role.voiceowl_nodegroup_role.arn
+  subnet_ids      = aws_subnet.voiceowl_subnet[*].id
 
   scaling_config {
     desired_size = 2
@@ -123,6 +151,6 @@ resource "aws_eks_node_group" "node_group" {
   instance_types = ["t3.medium"]
 
   depends_on = [
-    aws_eks_cluster.cluster,
+    aws_eks_cluster.voiceowl_cluster,
   ]
 }
